c0d22b1dd0337bb9636d3fa542e8299eef1d9376
Arya Korrapati <arya.korrapati@duke.edu>
2025-05-28 22:34:11 -0400

    test in testing

--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect('/eos/user/a/akorrapa/test_study.db')
-c = conn.cursor()
-c.execute('CREATE TABLE test (id INTEGER PRIMARY KEY)')
-conn.commit()
-conn.close()
+import optuna
+storage = "sqlite:////eos/user/a/akorrapa/test_study.db"
+study = optuna.create_study(study_name="quicktest", storage=storage, direction="maximize", load_if_exists=True)
+def objective(trial):
+    return trial.suggest_float("x", 0, 1)
+study.optimize(objective, n_trials=2)
